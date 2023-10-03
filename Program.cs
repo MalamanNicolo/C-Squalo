5956a9a6d89bd137cf3353d32b1eb6eaad994b6a
MalamanNicolo <121225853+MalamanNicolo@users.noreply.github.com>
2023-10-03 08:42:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -8,8 +8,34 @@ namespace Animale
 {
     class Program
     {
+        struct Animali
+        {
+            public string razza;
+            public string specie;
+            public override string ToString()
+            {
+                return String.Format($"{razza, -10}{specie,-10} ");
+            }
+
+
+        }
         static void Main(string[] args)
         {
+            Animali creature = new Animali() {razza = "Elefante", specie = "Mammifero" };
+            Animali[] zoo = { new Animali { razza = "Delfino", specie = "Mammifero" }, new Animali { razza = "Coccodrillo", specie = "Rettile" } };
+            Console.WriteLine($"Variabile prima del cambio {creature.ToString()}");
+            Console.WriteLine($"Array prima del cambio {zoo[0].ToString()}");
+            Cambia(ref creature, zoo);
+            Console.WriteLine($"Variabile dopo del cambio {creature.ToString()}");
+            Console.WriteLine($"Array dopo del cambio {zoo[0].ToString()}");
+            Console.ReadLine();
+        }
+        static void Cambia(ref Animali var, Animali[] arr)
+        {
+            var.razza = "serpente";
+            var.specie = "rettile";
+            arr[0].razza = "scimmia";
+            arr[0].specie = "mammifero";
         }
     }
 }
